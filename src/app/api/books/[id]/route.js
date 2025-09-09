73a86823070caa66d39b104d1de0a7ef8e11bc02
Laiba Mudassir <134529402+laiba2607@users.noreply.github.com>
2025-09-10 01:30:59 +0500

    Update route.js

--- src/app/api/books/[id]/route.js
+++ src/app/api/books/[id]/route.js
@@ -1,9 +1,10 @@
-export const dynamic = "force-dynamic"; // ⬅️ put this at the top of [id]/route.js
-
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
 import Book from "@/models/Book";
 import mongoose from "mongoose";
+
+export const dynamic = "force-dynamic"; // ⬅️ put this at the top of [id]/route.js
+
 
 export async function GET(request, { params }) {
   try {
